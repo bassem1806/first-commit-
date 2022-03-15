b7b25062d8b5853a6169adcad3baa22554888020
bassem ben dhief <bendhief.b@gmail.com>
2022-03-15 09:57:30 +0100

    premier commit

---
+++ src/formationOCA/chaptre1/Personne.java
@@ -0,0 +1,30 @@
+package formationOCA.chaptre1;
+
+public class Personne {
+	
+	// attribus d'intance
+	
+	String nom;
+	int age; 
+	double moyene;
+	Date 
+	
+	public Personne (String n)
+	{
+		System.out.println("constructeur avec parametre");
+		
+	}
+	
+	public void info{
+		System.out.println("une personne");
+		
+	}
+	
+	
+	static void info{
+		System.out.println("une personne");
+		
+	}
+	
+
+}
---
+++ src/formationOCA/chaptre1/Test.java
@@ -0,0 +1,31 @@
+package formationOCA.chaptre1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		/*byte x =126;
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);*/
+		
+		/*System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);*/
+		
+		/*System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);*/
+		
+		/*
+		boolean res =true;
+		System.out.println("res=" +res);// message mixte une partie fixe est une partie qui change*/
+		
+		/*String formation ="bassem ben dhief";
+		System.out.println(formation.length());*/
+		
+		int _=1_000_000;
+		System.out.println(_);
+		
+		
+		
+		float m = 12.5F;
+	}
+
+}
